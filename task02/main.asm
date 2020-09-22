a2f8be0033a8d00235c9e024fefb097d2cd806c5
RuzikNF <ruslan.g2002@gmail.com>
2020-09-22 11:02:20 +0300

    feat: task02, negative integers input

--- task02/main.asm
+++ task02/main.asm
@@ -1,9 +1,19 @@
 format ELF64
 public _start
 section '.data' writable
+  DESCRIPTION db "Ruslan Garifullin (https://github.com/ruslang02)", 10, "From array A[N] create an array B[N] from odd elements.", 10, 10, 0
+  ARRAY_A db "Array A:", 10, 0
+  ARRAY_B db "Array B:", 10, 0
   ENTER_N db "Enter N: ", 0
+  INPUT_BEFORE db "[", 0
+  INPUT_AFTER db "] = ", 0
+  EMPTY_MSG db "(empty)", 10, 0
+  NEW_LINE db 10, 0
+  MINUS dq "-"
   N dq ?
-  ARRAY dd 10 dup(N)
+  R dq 0
+  ARRAY rq 100
+  OUTPUT_ARRAY rq 100
 section '.bss' writeable 
     _bss_char rb 1
     _buffer_char_size equ 2
@@ -13,26 +23,141 @@ section '.bss' writeable
     _buffer_number rb _buffer_number_size
 section '.code' executable
   _start:
+    mov rax, DESCRIPTION
+    call str_print
+
     mov rax, ENTER_N
     call str_print
-    mov rax, N
+
     call input_number
-    xor rcx, rcx
-    xor rdx, rdx
-    .next_item:
-      mov rax, qword [ARRAY + rcx]
-      call input_number
-      add rcx, 4
-      inc rdx
-      cmp rdx, qword [N]
-      jnz .next_item
+    mov [N], rax
+
+    call read_array
+
+    mov rax, ARRAY_A
+    call str_print
+    mov rax, ARRAY
+    mov rbx, [N]
+    call print_array
+
+    call process_array
+
+    mov rax, ARRAY_B
+    call str_print
+    mov rax, OUTPUT_ARRAY
+    mov rbx, [R]
+    call print_array
+
     call exit
     ret
+  
   exit:
     mov rax, 1
     mov rbx, 0
     int 80h
 
+section '.read_array' executable
+  ; | input
+  ; N - number of elements to read
+  read_array:
+    push rax
+    push rcx
+    xor rcx, rcx; counter
+    .next_item:
+      mov rax, INPUT_BEFORE
+      call str_print
+      mov rax, rcx
+      call print_number
+      mov rax, INPUT_AFTER
+      call str_print; [i] = ...
+
+      call input_number
+      mov [ARRAY + rcx * 8], rax
+      inc rcx; rcx++
+      cmp rcx, [N]; if (rcx == N) return;
+      jnz .next_item
+    pop rcx
+    pop rax
+    ret
+section '.print_array' executable
+  ; | input:
+  ; rax = pointer to array
+  ; rbx = array length
+  print_array:
+    push rax
+    push rbx
+    push rcx
+    push r8
+    push r9
+    mov r8, rax
+    mov r9, rbx
+    xor rcx, rcx; counter
+    .next_item:
+      cmp rcx, r9; if (rcx == N) return;
+      jge .return
+
+      mov rax, INPUT_BEFORE
+      call str_print
+      mov rax, rcx
+      call print_number
+      mov rax, INPUT_AFTER
+      call str_print; [i] = ...
+
+      mov rax, [r8 + rcx * 8]
+      call print_number
+      mov rax, NEW_LINE
+      call str_print
+
+      inc rcx; rcx++
+      jmp .next_item
+    .return:
+      cmp rcx, 0
+      je .empty_msg
+    .clear:
+      pop r9
+      pop r8
+      pop rcx
+      pop rbx
+      pop rax
+      ret
+    .empty_msg:
+      mov rax, EMPTY_MSG
+      call str_print
+      jmp .clear
+
+section '.process_array' executable
+  process_array:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push r8
+    push r9
+    mov rcx, 0; counter
+    xor r8, r8; counter for output array
+    .next_item:
+      cmp rcx, [N]
+      jge .return
+      mov rax, [ARRAY + rcx * 8]
+      mov r9, rax
+      mov rbx, 2
+      div rbx
+      inc rcx
+      cmp rdx, 0
+      je .next_item
+    .push_array:
+      mov [OUTPUT_ARRAY + r8 * 8], r9
+      inc r8
+      jmp .next_item
+    .return:
+      mov [R], r8
+      pop r9
+      pop r8
+      pop rdx
+      pop rcx
+      pop rbx
+      pop rax
+      ret
 section '.str_func' executable
   str_len:
     xor rbx, rbx
@@ -43,6 +168,10 @@ section '.str_func' executable
     ret
   str_print:
     push rax
+    push rbx
+    push rcx
+    push rdx
+    push rax
     call str_len
     push rbx
     mov rax, 4
@@ -50,8 +179,56 @@ section '.str_func' executable
     pop rdx
     pop rcx
     int 80h
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
     ret
 
+section '.print_number' executable
+; | input:
+; rax = number
+print_number:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    xor rcx, rcx
+    ;cmp rax, 0
+    ;jl .toggle_invert
+    .next_iter:
+        mov rbx, 10
+        xor rdx, rdx
+        div rbx
+        add rdx, '0'
+        push rdx
+        inc rcx
+        cmp rax, 0
+        je .print_iter
+        jmp .next_iter
+    .print_iter:
+        cmp rcx, 0
+        je .close
+        pop rax
+        call print_char
+        dec rcx
+        jmp .print_iter
+    ;.toggle_invert:
+    ;    push rax
+    ;    mov rax, 45
+    ;    call print_char
+    ;    pop rax
+    ;    push rbx
+    ;    mov rbx, -1
+    ;     imul rbx
+    ;    pop rbx
+    ;    jmp .next_iter
+    .close:
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rax
+        ret
 section '.input_string' executable
 ; | input:
 ; rax = buffer
@@ -77,6 +254,29 @@ input_string:
     pop rax
     ret
 
+section '.print_char' executable
+; | input
+; rax = char
+print_char:
+    push rdx
+    push rcx
+    push rbx
+    push rax
+
+    mov [_bss_char], al
+
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, _bss_char
+    mov rdx, 1
+    int 0x80
+
+    pop rax
+    pop rbx
+    pop rcx
+    pop rdx
+    ret
+
 section '.input_number' executable
 ; | output:
 ; rax = number
@@ -98,8 +298,12 @@ string_to_number:
     push rbx
     push rcx
     push rdx
+    push r11
     xor rbx, rbx
     xor rcx, rcx
+    xor r11, r11
+    cmp [rax], byte '-'
+    je .toggle_invert
     .next_iter:
         cmp [rax+rbx], byte 0
         je .next_step
@@ -114,13 +318,27 @@ string_to_number:
     .to_number:
         cmp rbx, 0
         je .close
+        cmp r11, 1
+        je .invert
         pop rdx
         imul rdx, rcx
         imul rcx, 10
         add rax, rdx
         dec rbx
         jmp .to_number
+    .toggle_invert:
+        mov r11, 1
+        mov [rax], byte '0'
+        jmp .next_iter
+    .invert:
+        push rbx
+        xor r11, r11
+        mov rbx, -1
+        imul rbx
+        pop rbx
+        jmp .to_number
     .close:
+        pop r11
         pop rdx
         pop rcx
         pop rbx
