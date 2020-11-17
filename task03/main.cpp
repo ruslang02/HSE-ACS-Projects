e772b26bcac838396c3ae8e14fc440f9ec1ba1e4
RuzikNF <ruslan.g2002@gmail.com>
2020-11-17 14:41:13 +0300

    feat: finished task03

--- task03/main.cpp
+++ task03/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <pthread.h>
+#include "pthread.h"
 
 const int maxSize = 100;
 
@@ -15,11 +15,12 @@ struct ThreadData
   int lenC;
 };
 
-bool exists(int *array, int element)
+bool exists(int *array, int element, int arrLength)
 {
-  for (int i = 0; i < sizeof(array) / sizeof(int); i++)
+  for (int i = 0; i < arrLength; i++)
   {
-    if (*(array + i) == element)
+    std::cout << array[i] << "=" << element << " ";
+    if (array[i] == element)
       return true;
   }
   return false;
@@ -28,40 +29,195 @@ bool exists(int *array, int element)
 void *andProcess(void *args)
 {
   ThreadData *data = (ThreadData *)args;
-  int lenA = sizeof(data->A) / sizeof(int), lenB = sizeof(data->B) / sizeof(int), lenC = sizeof(data->C) / sizeof(int);
-  int andSet[data->lenA < data->lenB ? data->lenA : data->lenB];
-  int index = 0;
-  for (int i = 0; i < data->lenA; i++)
+  int *A = (int *)data->A;
+  int *B = (int *)data->B;
+  int *C = (int *)data->C;
+  int lenA = data->lenA;
+  int lenB = data->lenB;
+  int lenC = data->lenC;
+  int AND[lenA > lenB ? lenA : lenB];
+  int index = 0, i = 0, j = 0;
+  while (i < lenA && j < lenB)
   {
-    if (exists(data->A, data->B[i]))
-      andSet[index++] = data->B[i];
-  }
-  for (int i = 0; i < data->lenB; i++)
-  {
-    if (exists(data->B, data->A[i]) && !exists(andSet, data->A[i]))
-      andSet[index++] = data->A[i];
+    if (A[i] == B[j])
+    {
+      if (index == 0 || AND[index - 1] != A[i])
+      {
+        AND[index] = A[i];
+        index++;
+      }
+      i++;
+      j++;
+    }
+    else if (A[i] < B[j])
+      i++;
+    else
+      j++;
   }
 
-  if (index != data->lenC)
-  {
+  if (index != lenC)
     return (void *)false;
-  }
+
+  for (int i = 0; i < index; i++)
+    if (AND[i] != C[i])
+      return (void *)false;
+
   return (void *)true;
 }
 
 void *orProcess(void *args)
 {
   ThreadData *data = (ThreadData *)args;
+  int *A = (int *)data->A;
+  int *B = (int *)data->B;
+  int *C = (int *)data->C;
+  int lenA = data->lenA;
+  int lenB = data->lenB;
+  int lenC = data->lenC;
+  int OR[lenA + lenB];
+  int index = 0, i = 0, j = 0;
+  while (i < lenA && j < lenB)
+  {
+    if (A[i] < B[j])
+    {
+      if (OR[index - 1] != A[i])
+      {
+        OR[index] = A[i];
+        index++;
+      }
+      i++;
+    }
+    else if (B[j] < A[i])
+    {
+      if (OR[index - 1] != B[j])
+      {
+        OR[index] = B[j];
+        index++;
+      }
+      j++;
+    }
+    else
+    {
+      if (OR[index - 1] != B[j])
+      {
+        OR[index] = B[j++];
+        index++;
+      }
+      i++;
+    }
+  }
+  while (i < lenA)
+  {
+    if (OR[index - 1] != A[i])
+    {
+      OR[index] = A[i];
+      index++;
+    }
+    i++;
+  }
+  while (j < lenB)
+  {
+    if (OR[index - 1] != B[j])
+    {
+      OR[index] = B[j];
+      index++;
+    }
+    j++;
+  }
 
-  int orSet[]
+  if (index != lenC)
+    return (void *)false;
+
+  for (int i = 0; i < index; i++)
+    if (OR[i] != C[i])
+      return (void *)false;
+
+  return (void *)true;
 }
 
-void *rcofAinBProcess(void *args) {
+void *rcofAinBProcess(void *args)
+{
   ThreadData *data = (ThreadData *)args;
+  int *A = (int *)data->A;
+  int *B = (int *)data->B;
+  int *C = (int *)data->C;
+  int lenA = data->lenA;
+  int lenB = data->lenB;
+  int lenC = data->lenC;
+  int AinB[lenB];
+  int index = 0;
+  for (int i = 0; i < lenB; i++)
+  {
+    bool flag = false;
+    for (int j = 0; j < lenA; j++)
+    {
+      if (A[j] == B[i])
+        flag = true;
+    }
+    if (!flag && AinB[index - 1] != B[i])
+      AinB[index++] = B[i];
+  }
+
+  if (index != lenC)
+    return (void *)false;
+
+  for (int i = 0; i < index; i++)
+    if (AinB[i] != C[i])
+      return (void *)false;
+
+  return (void *)true;
 }
 
-void *rcofBinAProcess(void *args) {
+void *rcofBinAProcess(void *args)
+{
   ThreadData *data = (ThreadData *)args;
+  int *A = (int *)data->A;
+  int *B = (int *)data->B;
+  int *C = (int *)data->C;
+  int lenA = data->lenA;
+  int lenB = data->lenB;
+  int lenC = data->lenC;
+  int BinA[lenB];
+  int index = 0;
+  for (int i = 0; i < lenA; i++)
+  {
+    bool flag = false;
+    for (int j = 0; j < lenB; j++)
+    {
+      if (B[j] == A[i])
+        flag = true;
+    }
+    if (!flag && BinA[index - 1] != A[i])
+      BinA[index++] = A[i];
+  }
+
+  if (index != lenC)
+    return (void *)false;
+
+  for (int i = 0; i < index; i++)
+    if (BinA[i] != C[i])
+      return (void *)false;
+
+  return (void *)true;
+}
+
+int inputSet(std::string line, int *set, int size)
+{
+  std::istringstream stream(line);
+  for (int i = 0; i < size; i++)
+  {
+    if (!stream.good())
+      return 1;
+    int tmp;
+    stream >> tmp;
+    if (i != 0 && set[i - 1] > tmp)
+    {
+      std::cerr << "Set must be sorted.\n";
+      return 1;
+    }
+    set[i] = tmp;
+  }
+  return 0;
 }
 
 int main(int argc, char **argv)
@@ -79,6 +235,16 @@ int main(int argc, char **argv)
   if (input.good() && std::getline(input, line))
   {
     std::istringstream(line) >> lenA >> lenB >> lenC;
+    if (lenA <= 0 || lenB <= 0 || lenC <= 0)
+    {
+      std::cerr << "Set cannot be empty.\n";
+      return 1;
+    }
+    if (lenA > 1000 || lenB > 1000 || lenC > 1000)
+    {
+      std::cerr << "Set cannot contain more than 1000 elements.\n";
+      return 1;
+    }
   }
   else
   {
@@ -86,43 +252,30 @@ int main(int argc, char **argv)
     return 1;
   }
   int A[lenA], B[lenB], C[lenC];
-  if (input.good() && std::getline(input, line))
-  {
-    std::istringstream stream(line);
-    for (int i = 0; i < lenA && stream >> A[i]; i++)
-      ;
-  }
+
+  if (input.good() && std::getline(input, line) && !inputSet(line, A, lenA))
+    std::cout << "Set A loaded.\n";
   else
   {
-    std::cerr << "Set A was not defined.\n";
+    std::cerr << "Invalid data given to set A.\n";
+    return 1;
+  }
+  if (input.good() && std::getline(input, line) && !inputSet(line, B, lenB))
+    std::cout << "Set B loaded.\n";
+  else
+  {
+    std::cerr << "Invalid data given to set B.\n";
     return 1;
   }
 
-  if (input.good() && std::getline(input, line))
-  {
-    std::istringstream stream(line);
-    for (int i = 0; i < lenB && stream >> B[i]; i++)
-      ;
-  }
+  if (input.good() && std::getline(input, line) && !inputSet(line, C, lenC))
+    std::cout << "Set C loaded.\n";
   else
   {
-    std::cerr << "Set B was not defined.\n";
+    std::cerr << "Invalid data given to set C.\n";
     return 1;
   }
 
-  if (input.good() && std::getline(input, line))
-  {
-    std::istringstream stream(line);
-    for (int i = 0; i < lenC && stream >> C[i]; i++)
-      ;
-  }
-  else
-  {
-    std::cerr << "Set C was not defined.\n";
-    return 1;
-  }
-
-  pthread_t pth_and, pth_or, pth_subB, pth_subA;
   ThreadData data;
   data.A = A;
   data.B = B;
@@ -130,11 +283,27 @@ int main(int argc, char **argv)
   data.lenA = lenA;
   data.lenB = lenB;
   data.lenC = lenC;
-  pthread_create(&pth_and, NULL, andProcess, &data);
-  for (int i = 0; i < sizeof(A) / sizeof(int); i++)
-  {
-    output << A[i] << " ";
-  }
+
+  pthread_t thread_and, thread_or, thread_ainb, thread_bina;
+  pthread_create(&thread_and, nullptr, andProcess, (void *)&data);
+  pthread_create(&thread_or, nullptr, orProcess, (void *)&data);
+  pthread_create(&thread_ainb, nullptr, rcofAinBProcess, (void *)&data);
+  pthread_create(&thread_bina, nullptr, rcofBinAProcess, (void *)&data);
+
+  bool andResult, orResult, ainbResult, binaResult;
+  pthread_join(thread_and, (void **)&andResult);
+  pthread_join(thread_or, (void **)&orResult);
+  pthread_join(thread_ainb, (void **)&ainbResult);
+  pthread_join(thread_bina, (void **)&binaResult);
+
+  output << "C is " << (andResult ? "" : "NOT ")
+         << "an intersection of sets A and B\n"
+         << "C is " << (orResult ? "" : "NOT ")
+         << "a union of sets A and B\n"
+         << "C is " << (ainbResult ? "" : "NOT ")
+         << "a relative complement of A in B\n"
+         << "C is " << (binaResult ? "" : "NOT ")
+         << "a relative complement of B in A";
 
   return 0;
 }
--- task03/test.js
+++ task03/test.js
@@ -1,62 +1,59 @@
 const child_process = require('child_process');
 const fs = require('fs');
+const { join } = require('path');
 const { readFile } = fs.promises;
 
-const TEST_COUNT = 1;
+const TEST_COUNT = 5;
+const COMPILER = 'c++ main.cpp -lpthread -o main';
+const INPUT_DIR = join(__dirname, 'test', 'input');
+const OUTPUT_DIR = join(__dirname, 'test', 'output');
 
 function compile() {
-    try {
-        const compile = child_process.execSync('c++ main.cpp -o main -pthread').toString();
-        if (compile) console.log(`Compiler output:\n${compile}`);
-        return true;
-    } catch (e) {
-        return false;
-    }
+  try {
+    const compile = child_process.execSync(COMPILER).toString();
+    if (compile) console.log(`Compiler output:\n${compile}`);
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
 
 function test() {
-    console.log(new Array(100).join('\n'));
-    console.log(`💽 Compiling...`)
-    if (!compile()) return console.log(`❌ Compilation failed.`);
-    console.log(`💽 Test started at ${new Date().toLocaleTimeString()}`)
-    for (let i = 1; i <= TEST_COUNT; i++) {
-        printTestRunning(i);
-        const time = Date.now();
-        const proc = child_process.spawn('./main', [
-            `./test/input/${i}.txt`,
-            `./test/output/${i}.tmp`
-        ]);
-        let result = '';
-        proc.stdout.on('data', (chunk) => result += chunk.toString());
-        proc.stderr.on('data', (chunk) => result += chunk.toString());
-        proc.on('close', async (code) => {
-            if (code != 0) return printTestFailed(i, `Code: ${code}.\nOutput: ${result}`);
-            const expected = await readFile(`./test/output/${i}.txt`);
-            const given = await readFile(`./test/output/${i}.tmp`);
-            if (expected != given) return printTestFailed(i, `Expected output:\n"${expected}"\n\nGiven output: "${given}"`);
-            printTestSuccessful(i, Date.now() - time);
-        });
+  console.log(new Array(100).join('\n'));
+  console.log(`💽 Compiling...`)
+  if (!compile()) return console.log(`❌ Compilation failed.`);
+  console.log(`💽 Test started at ${new Date().toLocaleTimeString()}`)
+  for (let i = 1; i <= TEST_COUNT; i++) runTest(i);
+}
+
+function runTest(i) {
+  const INPUT_FILE = join(INPUT_DIR, `${i}.txt`);
+  const OUTPUT_FILE = join(OUTPUT_DIR, `${i}.txt`);
+  const OUTPUT_TEMP_FILE = join(OUTPUT_DIR, `${i}.tmp`);
+  const time = Date.now();
+
+  console.log(`🥚 Running test #${i}...`);
+  const proc = child_process.spawn('./main', [INPUT_FILE, OUTPUT_TEMP_FILE]);
+  let result = '';
+  proc.stdout.on('data', (chunk) => result += chunk.toString());
+  proc.stderr.on('data', (chunk) => result += chunk.toString());
+  proc.on('close', async (code) => {
+    try {
+      if (code != 0) return printTestFailed(i, `Code: ${code}.\nOutput: ${result}`);
+      const expected = (await readFile(OUTPUT_FILE)).toString();
+      const given = (await readFile(OUTPUT_TEMP_FILE)).toString();
+      if (expected != given) return console.log(`❌ Test #${i} failed.`, `
+Expected output:
+${expected}
+
+Given output:
+${given}`);
+      console.log(`✅ Test #${i} finished successfully in ${Date.now() - time}ms.`);
+    } catch (e) {
+      console.log(`❌ Test #${i} failed.`, 'No output file was generated.');
     }
-}
-
-function printTestRunning(i) {
-    console.log(`🥚 Running test #${i}...`);
-}
-
-function printTestSuccessful(i, time) {
-    console.log(`✅ Test #${i} finished successfully in ${time}.`);
-}
-
-function printTestFailed(i, message) {
-    console.log(`❌ Test #${i} failed.`);
-    console.log(message);
+  });
 }
 
 test();
-fs.watchFile('./main.cpp', {persistent: true, interval: 100}, function () {
-    // if (timer) return;
-    // timer = setTimeout(() => {
-        test();
-    //    timer = undefined;
-    // }, 500);
-});
+fs.watchFile('./main.cpp', { persistent: true, interval: 100 }, test);
